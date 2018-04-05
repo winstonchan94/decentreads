3c4721b68e5fa1822b5d02c3aecbc1a68bcd4064
Winston Chan <winston.chan94@gmail.com>
2018-04-05 13:35:29 -0700

    Re-add app.jsx

---
+++ frontend/components/app.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import NavBarContainer from './navigation/nav_bar_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <NavBarContainer />
+      </header>
+      <Switch>
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
