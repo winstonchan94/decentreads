918d87bcb90faca29537fe9a0b7810ef58f0fb79
Winston Chan <winston.chan94@gmail.com>
2018-04-06 13:47:27 -0700

    Fix css

--- frontend/components/book/book_detail.jsx
+++ frontend/components/book/book_detail.jsx
@@ -7,7 +7,8 @@ const BookDetail = (book) => {
         <h2 className="book-title">{book.title}</h2>
         <h3 className="book-author">by {book.author.name}</h3>
       </header>
-
+      <p className="book-description">{book.description}</p>
+      <h5 className="book-publish-info">Published {book.publish_date} by {book.publisher}</h5>
     </div>
   );
 };
