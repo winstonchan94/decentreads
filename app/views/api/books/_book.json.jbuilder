5e7f5271a8153a89c460c0a25ebdb9bbb0a4f422
Winston Chan <winston.chan94@gmail.com>
2018-04-11 07:03:43 -0700

    Do stuff

--- app/views/api/books/_book.json.jbuilder
+++ app/views/api/books/_book.json.jbuilder
@@ -1,4 +1,5 @@
 json.extract! book,
-  :id, :title, :author_id, :description,
+  :id, :title, :description,
   :publisher, :publish_date, :author
 json.coverUrl book.cover_url
+json.authorId book.author_id
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import SignupFormContainer from './session_form/signup_form_container';
 import BookShowContainer from './book/book_show_container';
 import BookIndexContainer from './book/book_index_container';
 import SideBarContainer from './navigation/side_bar_container';
+import ShelfShowContainer from './shelf/shelf_show_container';
 import { Provider } from 'react-redux';
 import {
   Route,
@@ -28,6 +29,7 @@ const App = () => {
         <Route exact path="/" component={BookIndexContainer} />
         <Route path="/books/:bookId" component={BookShowContainer} />
         <ProtectedRoute path="/mybooks" component={SideBarContainer}/>
+        <Route path="/mybooks/shelf/:shelfId" component={ShelfShowContainer}/>
       </Switch>
       <footer><li>© 2018 DecentReads Inc</li></footer>
     </div>
