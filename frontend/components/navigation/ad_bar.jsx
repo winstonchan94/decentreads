87239762b34edd528e34488fce9bbd2eee5de60b
Winston Chan <winston.chan94@gmail.com>
2018-08-27 11:44:30 -0700

    Add clickbaity links to profiles - finished

--- frontend/components/navigation/ad_bar.jsx
+++ frontend/components/navigation/ad_bar.jsx
@@ -6,9 +6,16 @@ class AdBar extends React.Component {
   render() {
     return (
         <ul className="ad-list">
-          <li>
+          <li className="ad-list-item">
             <a className="ad-list-link" href="https://www.linkedin.com/in/winstonchan94/">
+              <img className="clickbait-img" src="https://res.cloudinary.com/ddcscckq0/image/upload/v1535395049/clickbait.jpg"></img>
               Scientists baffled by the quality of this developer's LinkedIn profile!
+            </a>
+          </li>
+          <li className="ad-list-item">
+            <a className="ad-list-link" href="https://github.com/winstonchan94">
+              <img className="clickbait-img" src="https://res.cloudinary.com/ddcscckq0/image/upload/v1535395049/clickbait2.webp"></img>
+              Countless others have drawn inspiration from this programmer's Github!
             </a>
           </li>
         </ul>
