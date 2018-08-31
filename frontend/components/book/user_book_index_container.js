192f850c7287aa557ab782aaae014083d40a7ce8
Winston Chan <winston.chan94@gmail.com>
2018-08-30 18:47:48 -0700

    SHingle

--- frontend/components/book/user_book_index_container.js
+++ frontend/components/book/user_book_index_container.js
@@ -15,7 +15,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   requestBooks: () => dispatch(requestBooks())
 });
-
+//ss
 export default connect(
   mapStateToProps,
   mapDispatchToProps
