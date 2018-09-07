e562744d8e66b0b3cd2aa68e45d0f8f1f4f3a341
Winston Chan <winston.chan94@gmail.com>
2018-09-06 22:47:45 -0700

    Fix issues

--- frontend/components/book/user_book_index_container.js
+++ frontend/components/book/user_book_index_container.js
@@ -15,7 +15,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   requestBooks: () => dispatch(requestBooks())
 });
-//ssssss
+//sssssss
 export default connect(
   mapStateToProps,
   mapDispatchToProps
